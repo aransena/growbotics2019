6665b92f01039cd372721147926a36514fd98eb1
aransena <aransena@gmail.com>
2019-04-10 16:35:31 +0100

    motor speed control

--- motor_control_L298/motor_control_L298.ino
+++ motor_control_L298/motor_control_L298.ino
@@ -9,7 +9,7 @@ static int m2_s = 6;
 void backward(){
   digitalWrite(m1_1, LOW);
   digitalWrite(m1_2, HIGH);
-
+  
   digitalWrite(m2_1, LOW);
   digitalWrite(m2_2, HIGH);
 }
@@ -18,6 +18,26 @@ void forward(){
   digitalWrite(m1_1, HIGH);
   digitalWrite(m1_2, LOW);
 
+  digitalWrite(m2_1, HIGH);
+  digitalWrite(m2_2, LOW);
+}
+
+void backward(int speed){
+  analogWrite(m1_s, speed);
+  digitalWrite(m1_1, LOW);
+  digitalWrite(m1_2, HIGH);
+  
+  analogWrite(m2_s, speed);
+  digitalWrite(m2_1, LOW);
+  digitalWrite(m2_2, HIGH);
+}
+
+void forward(int speed){
+  analogWrite(m1_s, speed);
+  digitalWrite(m1_1, HIGH);
+  digitalWrite(m1_2, LOW);
+
+  analogWrite(m2_s, speed);
   digitalWrite(m2_1, HIGH);
   digitalWrite(m2_2, LOW);
 }
@@ -43,9 +63,10 @@ int i = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  forward();
-  analogWrite(m1_s, i);
-  analogWrite(m2_s, i);
+//  forward();
+  forward(i);
+//  analogWrite(m1_s, i);
+//  analogWrite(m2_s, i);
   i = i+10;
   i = min(255,i);
   delay(50 );
